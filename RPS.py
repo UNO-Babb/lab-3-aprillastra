e0a88312d2021b1dd074b256991f75b714cba110
aprillastra <klastra-bernal@unomaha.edu>
2025-02-10 00:21:25 +0000

    RPS game

--- RPS.py
+++ RPS.py
@@ -10,9 +10,24 @@ def main():
   losses = 0
   #Create a loop that continues as long as the user wants to play.
   #User can play as many games as they wish.
-
+  computer = random.choice( ["R", "P", "S"])
+  User_choice = input("Enter R for Rock, P for Paper, or S for Scissors (or Q to Quit): ").upper()
+  if user_choice == 'Q': break 
   #Randomly choose the computer between 'R', 'P', or 'S'
+  if user_choice is not [ 'R', 'P', 'S']:
+    print("Invalid choice")
   #Prompt the user for their RPS selection
+  if user_choice == computer_choice: 
+    print("It's a tie.")
+    ties = ties + 1
+    elif (user_choice == 'R' and computer_choice == 'S'); or \
+        (user_choice == 'P' and computer_choice == 'R') or \
+        (user_choice == 'S' and computer_choice == 'P'): 
+  print("you win!! {user_choice} beats {computer_choice}.")
+  wins = wins +1
+ else: 
+print( "You lose! {computer_choice} beats {user_choice}.")
+losses = losses + 1 
   #Determine winner and state what happened to the user
   #Ask the user if they would like to play again.
 
